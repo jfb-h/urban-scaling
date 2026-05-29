3d25b9031456981df9de905fcf3066574dbd3efd
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2026-05-29 09:19:34 +0200

    init

---
+++ build/build-dataset.R
@@ -0,0 +1,110 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+
+gdp_raw <- read_csv("data/raw/tl3-gdp.csv")
+pop_raw <- read_csv("data/raw/tl3-pop.csv")
+emp_raw <- read_csv("data/raw/tl3-emp.csv")
+
+pop <- pop_raw |>
+  select(
+    territorial_level = TERRITORIAL_LEVEL,
+    code = REF_AREA,
+    country = COUNTRY,
+    name = `Reference area`,
+    time = TIME_PERIOD,
+    unit = UNIT_MEASURE,
+    pop = OBS_VALUE,
+    age = Age,
+    sex = Sex
+  ) |>
+  filter(
+    territorial_level == "TL3",
+    unit == "PS",
+    age == "Total",
+    sex == "Total"
+  ) |>
+  select(-territorial_level, -unit, -age, -sex)
+
+gdp <- gdp_raw |>
+  select(
+    territorial_level = TERRITORIAL_LEVEL,
+    code = REF_AREA,
+    country = COUNTRY,
+    name = `Reference area`,
+    time = TIME_PERIOD,
+    measure = MEASURE,
+    prices = PRICES,
+    unit = UNIT_MEASURE,
+    gdp = OBS_VALUE
+  ) |>
+  filter(
+    prices == "V",
+    measure == "GDP",
+    unit %in% c("USD_PPP", "USD_PPP_PS"),
+    territorial_level == "TL3"
+  ) |>
+  select(-c(measure, prices, territorial_level)) |>
+  pivot_wider(
+    names_from = unit,
+    values_from = gdp
+  ) |>
+  rename(
+    gdp = USD_PPP,
+    gdp_pc = USD_PPP_PS
+  )
+
+emp <- emp_raw |>
+  select(
+    code = REF_AREA,
+    territorial_level = TERRITORIAL_LEVEL,
+    country = COUNTRY,
+    name = `Reference area`,
+    time = TIME_PERIOD,
+    activity_code = ACTIVITY,
+    activity = `Economic activity`,
+    unit = UNIT_MEASURE,
+    emp = OBS_VALUE
+  ) |>
+  filter(
+    unit == "PS",
+    territorial_level == "TL3"
+  ) |>
+  select(-c(unit, territorial_level))
+
+
+dat <- left_join(pop, gdp)
+dat <- left_join(dat, emp)
+
+dat <- dat |>
+  select(-activity) |>
+  pivot_wider(
+    names_from = activity_code,
+    values_from = emp,
+    names_prefix = "emp_"
+  )
+
+
+arrow::write_parquet(dat, "data/processed/data.parquet")
+
+# test analysis
+
+library(ggplot2)
+
+theme_set(hrbrthemes::theme_ipsum_pub())
+
+countries <- c("USA", "DEU", "FRA", "ITA", "NLD", "POL")
+
+pd <- filter(dat, time == 2020, country %in% countries)
+
+ggplot(pd, aes(pop, gdp)) +
+  geom_point(size = 1, alpha = 0.3) +
+  geom_smooth(method = "lm") +
+  scale_x_log10() +
+  scale_y_log10()
+
+ggplot(pd, aes(pop, gdp_pc)) +
+  geom_point(size = 0.7) +
+  geom_smooth(method = "lm") +
+  scale_x_log10() +
+  facet_wrap(~country, scales = "free")
--- justfile
+++ justfile
@@ -1,2 +1,5 @@
 default:
     @just --list -u
+
+build:
+  Rscript --vanilla build/build-dataset.R
